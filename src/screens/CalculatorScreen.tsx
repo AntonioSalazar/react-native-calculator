ca2793ae09718187bd2d3656d34663cfd7a8ed4c
unknown <antonio.salazar.emrsn@gmail.com>
2021-06-15 00:25:33 -0500

    working on the operation buttons

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,14 +1,24 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {View, Text} from 'react-native';
 import {styles} from '../themes/appTheme';
 import CalcButton from '../components/CalcButton';
+
+enum Operations {
+  add,
+  substract,
+  multiplication,
+  division,
+}
 
 const CalculatorScreen = () => {
   const [previousNumber, setPreviousNumber] = useState('0');
   const [number, setNumber] = useState('0');
 
+  const lasOperation = useRef<Operations>();
+
   const clean = () => {
     setNumber('0');
+    setPreviousNumber('0');
   };
 
   const btnDelete = () => {
@@ -53,9 +63,40 @@ const CalculatorScreen = () => {
     }
   };
 
+  const addPreviousNumber = () => {
+    if (number.endsWith('.')) {
+      setPreviousNumber(number.slice(0, -1));
+    } else {
+      setPreviousNumber(number);
+    }
+    setNumber('0');
+  };
+
+  const divisionBtn = () => {
+    addPreviousNumber();
+    lasOperation.current = Operations.division;
+  };
+
+  const multiplicationBtn = () => {
+    addPreviousNumber();
+    lasOperation.current = Operations.multiplication;
+  };
+
+  const substractBtn = () => {
+    addPreviousNumber();
+    lasOperation.current = Operations.substract;
+  };
+
+  const addBtn = () => {
+    addPreviousNumber();
+    lasOperation.current = Operations.add;
+  };
+
   return (
     <View style={styles.calculatorContainer}>
-      <Text style={styles.smallResult}>{previousNumber}</Text>
+      {previousNumber !== '0' && (
+        <Text style={styles.smallResult}>{previousNumber}</Text>
+      )}
       <Text style={styles.result} numberOfLines={1} adjustsFontSizeToFit>
         {number}
       </Text>
@@ -67,27 +108,27 @@ const CalculatorScreen = () => {
           action={togglePositiveAndNegative}
         />
         <CalcButton text="del" color="#9b9b9b" action={btnDelete} />
-        <CalcButton text="/" color="#ff9427" action={clean} />
+        <CalcButton text="/" color="#ff9427" action={divisionBtn} />
       </View>
 
       <View style={styles.row}>
         <CalcButton text="7" action={saveNumber} />
         <CalcButton text="8" action={saveNumber} />
         <CalcButton text="9" action={saveNumber} />
-        <CalcButton text="X" color="#ff9427" action={clean} />
+        <CalcButton text="X" color="#ff9427" action={multiplicationBtn} />
       </View>
 
       <View style={styles.row}>
         <CalcButton text="4" action={saveNumber} />
         <CalcButton text="5" action={saveNumber} />
         <CalcButton text="6" action={saveNumber} />
-        <CalcButton text="-" color="#ff9427" action={clean} />
+        <CalcButton text="-" color="#ff9427" action={substractBtn} />
       </View>
       <View style={styles.row}>
         <CalcButton text="1" action={saveNumber} />
         <CalcButton text="2" action={saveNumber} />
         <CalcButton text="3" action={saveNumber} />
-        <CalcButton text="+" color="#ff9427" action={clean} />
+        <CalcButton text="+" color="#ff9427" action={addBtn} />
       </View>
       <View style={styles.row}>
         <CalcButton text="0" width action={saveNumber} />

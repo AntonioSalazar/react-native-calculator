9b39f2e3b29a8c65da41137183c8e372e0c819f1
unknown <antonio.salazar.emrsn@gmail.com>
2021-06-14 22:16:37 -0500

    finished the design of the calc

--- src/components/CalcButton.tsx
+++ src/components/CalcButton.tsx
@@ -5,16 +5,24 @@ import {styles} from '../themes/appTheme';
 interface Props {
   text: string;
   color?: string;
+  width?: boolean;
 }
 
-const CalcButton = ({text, color}: Props) => {
+const CalcButton = ({text, color = '#2d2d2d', width = false}: Props) => {
   return (
     <View
       style={{
         ...styles.button,
         backgroundColor: color,
+        width: width ? 180 : 80,
       }}>
-      <Text style={styles.buttonText}>{text}</Text>
+      <Text
+        style={{
+          ...styles.buttonText,
+          color: color === '#9b9b9b' ? 'black' : 'white',
+        }}>
+        {text}
+      </Text>
     </View>
   );
 };
--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -9,10 +9,35 @@ const CalculatorScreen = () => {
       <Text style={styles.smallResult}>1500</Text>
       <Text style={styles.result}>1500</Text>
       <View style={styles.row}>
-        <CalcButton text="C" color="#9b9b9b9b" />
-        <CalcButton text="+/-" color="#9b9b9b9b" />
-        <CalcButton text="del" color="#9b9b9b9b" />
+        <CalcButton text="C" color="#9b9b9b" />
+        <CalcButton text="+/-" color="#9b9b9b" />
+        <CalcButton text="del" color="#9b9b9b" />
         <CalcButton text="/" color="#ff9427" />
+      </View>
+
+      <View style={styles.row}>
+        <CalcButton text="7" />
+        <CalcButton text="8" />
+        <CalcButton text="9" />
+        <CalcButton text="X" color="#ff9427" />
+      </View>
+
+      <View style={styles.row}>
+        <CalcButton text="4" />
+        <CalcButton text="5" />
+        <CalcButton text="6" />
+        <CalcButton text="-" color="#ff9427" />
+      </View>
+      <View style={styles.row}>
+        <CalcButton text="1" />
+        <CalcButton text="2" />
+        <CalcButton text="3" />
+        <CalcButton text="+" color="#ff9427" />
+      </View>
+      <View style={styles.row}>
+        <CalcButton text="0" width />
+        <CalcButton text="." />
+        <CalcButton text="=" color="#ff9427" />
       </View>
     </View>
   );
